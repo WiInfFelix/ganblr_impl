2b072db34209d82b31e588133012e986ccf9c059
Felix Scheffler <felix.scheffler@studium.uni-hamburg.de>
2023-10-10 08:29:05 +0200

    CTGAN base impl

--- ganblr_test.py
+++ ganblr_test.py
@@ -12,6 +12,8 @@ from logger_utils import CSVLogger
 from sklearn.model_selection import train_test_split
 from metric_utils import get_trtr_metrics, get_sdv_metrics
 from datetime import datetime
+from sdv.metadata import SingleTableMetadata
+from sdv.single_table import CTGANSynthesizer
 
 EPOCHS = [10, 25, 50, 100, 150]  
 K = [0, 1]  # , 2, 3, 4, 5
@@ -162,3 +164,106 @@ for epochs in EPOCHS:
             k=k,
             timestamp=timestamp,
         )
+        
+    superstore_metadata = SingleTableMetadata()
+    superstore_metadata.detect_from_dataframe(data=SUPERSTORE_DF_ENC)
+    superstore_ctgan = CTGANSynthesizer(superstore_metadata)
+    superstore_ctgan.fit(SUPERSTORE_DF_ENC)
+    
+    synth_data_superstore_ctgan = pd.DataFrame(
+        superstore_ctgan.sample(X_super.shape[0]),
+        columns=SUPERSTORE_DF_ENC.columns,
+    )
+    
+    get_trtr_metrics(
+        X_super_train,
+        X_super_test,
+        y_super_train,
+        y_super_test,
+        synth_data_superstore_ctgan,
+        "superstore",
+        "CTGAN",
+        csv_logger,
+        epochs,
+        0,
+    )
+    
+    get_sdv_metrics(
+        real_data=SUPERSTORE_DF_ENC,
+        synth_data=synth_data_superstore_ctgan,
+        dataset_name="superstore",
+        model="CTGAN",
+        csv_logger=csv_logger,
+        epochs=epochs,
+        k=0,
+        timestamp=timestamp,
+    )
+    
+    credit_risk_metadata = SingleTableMetadata()
+    credit_risk_metadata.detect_from_dataframe(data=CREDIT_RISK_DF_ENC)
+    credit_risk_ctgan = CTGANSynthesizer(credit_risk_metadata)
+    credit_risk_ctgan.fit(CREDIT_RISK_DF_ENC)
+    
+    synth_data_credit_risk_ctgan = pd.DataFrame(
+        credit_risk_ctgan.sample(X_credit.shape[0]),
+        columns=CREDIT_RISK_DF_ENC.columns,
+    )
+    
+    get_trtr_metrics(
+        X_credit_train,
+        X_credit_test,
+        y_credit_train,
+        y_credit_test,
+        synth_data_credit_risk_ctgan,
+        "credit_risk",
+        "CTGAN",
+        csv_logger,
+        epochs,
+        0,
+    )
+    
+    get_sdv_metrics(
+        real_data=CREDIT_RISK_DF_ENC,
+        synth_data=synth_data_credit_risk_ctgan,
+        dataset_name="credit_risk",
+        model="CTGAN",
+        csv_logger=csv_logger,
+        epochs=epochs,
+        k=0,
+        timestamp=timestamp,
+    )
+    
+    mushrooms_metadata = SingleTableMetadata()
+    mushrooms_metadata.detect_from_dataframe(data=MUSHROOMS_DF_ENC)
+    mushrooms_ctgan = CTGANSynthesizer(mushrooms_metadata)
+    mushrooms_ctgan.fit(MUSHROOMS_DF_ENC)
+    
+    synth_data_mushrooms_ctgan = pd.DataFrame(
+        mushrooms_ctgan.sample(X_mushrooms.shape[0]),
+        columns=MUSHROOMS_DF_ENC.columns,
+    )
+    
+    get_trtr_metrics(
+        X_mushrooms_train,
+        X_mushrooms_test,
+        y_mushrooms_train,
+        y_mushrooms_test,
+        synth_data_mushrooms_ctgan,
+        "mushrooms",
+        "CTGAN",
+        csv_logger,
+        epochs,
+        0,
+    )
+    
+    get_sdv_metrics(
+        real_data=MUSHROOMS_DF_ENC,
+        synth_data=synth_data_mushrooms_ctgan,
+        dataset_name="mushrooms",
+        model="CTGAN",
+        csv_logger=csv_logger,
+        epochs=epochs,
+        k=0,
+        timestamp=timestamp,
+    )
+    
